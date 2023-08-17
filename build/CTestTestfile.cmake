bd3641ba28ccc32c18cdd3f546bf5b47d7ec7091
Shyam Goli <golishyamgolisai@gmail.com>
2023-08-18 00:01:35 +0530

    Initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shyam/ROS_openCV/src
+# Build directory: /home/shyam/ROS_openCV/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_opencv_code")
---
+++ build/my_opencv_code/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shyam/ROS_openCV/src/my_opencv_code
+# Build directory: /home/shyam/ROS_openCV/build/my_opencv_code
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
